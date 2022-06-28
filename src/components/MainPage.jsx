0af3e7af02b9a31b35466cda1e47d9e6545c8779
Regina Grogan <regina.r.grogan@gmail.com>
2022-06-28 12:00:28 -0600

    Added search planets

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,13 +1,27 @@
+import {useState, useEffect} from 'react';
+
 //components
 import Planet from './Planet.jsx';
 
 export default function MainPage({planetsList}) {
+  const [searchedPlanets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    setPlanets(planetsList);
+  }, [planetsList])
+
+  let searchPlanetsOnChange = (e) => {
+    let searchedPlanet = e.target.value.toLowerCase();
+    let searchedPlanetsList = planetsList.filter((planet) => planet.name.toLowerCase().includes(searchedPlanet));
+    setPlanets(searchedPlanetsList);
+  }
 
   return (
     <div>
-        {planetsList.map((planet, index) => (
-          <Planet key={index} planet={planet}/>
-        ))}
+      <input onChange={searchPlanetsOnChange} type="text" id="search-planet" placeholder="Search Planet" name="search-planet" />
+      {searchedPlanets.map((planet, index) => (
+        <Planet key={index} planet={planet}/>
+      ))}
     </div>
   )
 }
